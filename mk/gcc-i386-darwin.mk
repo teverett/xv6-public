1c704b5f637e6875a5f412c7a23dc005eeb8af15
Tom Everett <tom@khubla.com>
2024-07-25 13:21:28 -0600

    build kinda works

--- mk/gcc-i386-darwin.mk
+++ mk/gcc-i386-darwin.mk
@@ -1,8 +1,8 @@
 
-CC=i386-elf-gcc
-LD=i386-elf-ld
-OBJCOPY=i386-elf-objcopy
-OBJDUMP=i386-elf-objdump
-AS=i386-elf-as
+CC=i686-elf-gcc
+LD=i686-elf-ld
+OBJCOPY=i686-elf-objcopy
+OBJDUMP=i686-elf-objdump
+AS=i686-elf-as
 QEMU=qemu-system-i386
-STRIP=i386-elf-strip
+STRIP=i686-elf-strip
